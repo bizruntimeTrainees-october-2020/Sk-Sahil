d5dcbf4944100afcd8df5710e3f1c580b1880383
sksahil152 <sksahil152@gmail.com>
2020-10-27 02:06:43 +0530

    jsonSerializer and xmlserializer

---
+++ serialization 09-10-2020/JsonSerialization/JsonSerialization/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { FirstName = "SK", LastName = "SAHIL" };
+            string filePath = @"E:\sahilJSON";
+            JSONSerializationAndDeserialization jSON
+                = new JSONSerializationAndDeserialization();
+            jSON.JsonSerialize(person, filePath);
+            Person p = jSON.JsonDeserialize(typeof(Person), filePath) as Person;
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            
+        }
+    }
+
+    class JSONSerializationAndDeserialization
+    {
+        public void JsonSerialize(object data, string filepath) 
+        {
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            if (File.Exists(filepath)) File.Delete(filepath);
+            StreamWriter sw = new StreamWriter(filepath);
+            JsonWriter jsonWriter = new JsonTextWriter(sw);
+            jsonSerializer.Serialize(jsonWriter, data);
+            jsonWriter.Close();
+            sw.Close();
+
+        }
+
+        public object JsonDeserialize(Type DataType ,string filepath) 
+        {
+           JObject jobj = null;
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            if (File.Exists(filepath)) 
+            {
+                StreamReader streamReader = new StreamReader(filepath);
+                JsonReader jsonReader = new JsonTextReader(streamReader);
+                jobj = jsonSerializer.Deserialize(jsonReader) as JObject;
+                jsonReader.Close();
+                streamReader.Close();
+            
+            } 
+            
+            return jobj.ToObject(DataType);
+        }
+
+    }
+
+    public class Person 
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ serialization 09-10-2020/XMLSerialization/XMLSerialization/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { FirstName = "sk", LastName = "sahil" };
+            string filepath = @"E:\xmlsahil";
+            XMLserialization xml = new XMLserialization();
+            xml.XMLSerialize(typeof(Person),person,filepath);
+            Person p = xml.XMLDeSerialize(typeof(Person), filepath) as Person;
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+        }
+    }
+
+
+    class XMLserialization
+    {
+        public void XMLSerialize(Type dataType, object data, string filepath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(dataType);
+            if (File.Exists(filepath)) File.Delete(filepath);
+            TextWriter writer = new StreamWriter(filepath);
+            xmlSerializer.Serialize(writer, data);
+            writer.Close();
+        }
+
+        public object XMLDeSerialize(Type dataType, string filepath) 
+        {
+            object obj = null;
+            XmlSerializer xmlSerializer = new XmlSerializer(dataType);
+            if (File.Exists(filepath)) 
+            {
+                TextReader textReader = new StreamReader(filepath);
+                obj = xmlSerializer.Deserialize(textReader);
+                textReader.Close();
+            }
+
+                return obj;
+        }   
+
+        
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
