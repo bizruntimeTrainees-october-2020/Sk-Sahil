7981c2e708043c4fae3024ba92ad43aa60a352cb
sksahil152 <sksahil152@gmail.com>
2020-10-27 02:40:35 +0530

    Use of DataSet

---
+++ Ado.net DataSet 13-10-2020/AdoNetDataSetConsole/AdoNetDataSetConsole/DatasetClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace AdoNetDataSetConsole
+{
+    class DatasetClass
+    {
+        DataTable dt;
+        DataColumn dc;
+        DataRow dr;
+        DataSet ds;
+
+        
+        public DataTable GetEmployeTable()
+        {
+            dt = new DataTable("Employee");
+            //employee tab region
+
+            dc = new DataColumn("eId", typeof(int));
+            dt.Columns.Add(dc);
+            dt.PrimaryKey = new DataColumn[] { dc };
+            dc = new DataColumn("eName", typeof(string));
+            dt.Columns.Add(dc);
+            dc = new DataColumn("dId", typeof(int));
+            dt.Columns.Add(dc);
+
+
+            //add rows
+
+            dr = dt.NewRow();
+            dr[0] = 1;
+            dr[1] = "Thiago Messi";
+            dr[2] = 10;
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr[0] = 2;
+            dr[1] = "Chritiano jr";
+            dr[2] = 15;
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr[0] = 3;
+            dr[1] = "Chettri jr";
+            dr[2] = 20;
+            dt.Rows.Add(dr);
+
+
+
+            return dt;
+        }
+        public DataTable GetDepartmentTable()
+        {
+            dt = new DataTable("Department");
+            //dept tab region
+
+            dc = new DataColumn("dId", typeof(int));
+            dt.Columns.Add(dc);
+            dt.PrimaryKey = new DataColumn[] { dc };
+            dc = new DataColumn("eName", typeof(string));
+            dt.Columns.Add(dc);
+            //add rows
+
+            dr = dt.NewRow();
+            dr[0] = 10;
+            dr[1] = "Hr Dept";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 20;
+            dr[1] = "Technical Dept";
+            dt.Rows.Add(dr);
+
+
+            return dt;
+        }
+        public DataSet GenerateDataSet()
+        {
+            DataTable emp = GetEmployeTable();
+            DataTable dept = GetDepartmentTable();
+            ds = new DataSet("MYDS");
+            ds.Tables.Add(emp);
+            ds.Tables.Add(dept);
+
+            //creating relationships
+           
+                DataColumn col_pk = ds.Tables["Department"].Columns["dId"];
+                DataColumn col_fk = ds.Tables["Employee"].Columns["dId"];
+                DataRelation drel = new DataRelation("Emp_Dept_relation", col_pk, col_fk);
+            try
+            {
+                ds.Relations.Add(drel);
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return ds;
+        }
+
+       
+       
+        
+   
+    }
+}
---
+++ Ado.net DataSet 13-10-2020/AdoNetDataSetConsole/AdoNetDataSetConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+namespace AdoNetDataSetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatasetClass dsc = new DatasetClass();
+            DataTable dataTable1= dsc.GetEmployeTable();
+           DataTable dataTable2= dsc.GetDepartmentTable();
+           DataSet dataSet= dsc.GenerateDataSet();
+
+
+            //printing dataset
+            
+                foreach (DataTable table in dataSet.Tables)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        foreach (DataColumn column in table.Columns)
+                        {
+                            Console.WriteLine(row[column]);
+                        }
+                    }
+                }
+
+
+
+
+        }
+    }
+}
