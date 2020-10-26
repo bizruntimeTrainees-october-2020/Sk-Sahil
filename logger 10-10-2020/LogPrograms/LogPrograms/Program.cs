8a760255a89a148dca1c28d378df5ba7687bc221
sksahil152 <sksahil152@gmail.com>
2020-10-27 02:21:22 +0530

    custom log file creating

---
+++ logger 10-10-2020/LogPrograms/LogPrograms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+namespace LogPrograms
+{
+  interface LogBase
+    {
+        public  void Log(string Messege);
+    }
+   public class Logger: LogBase
+    {
+        private string CurrentDirectory { get; set; }
+        private string FileName { get; set; }
+        private string FilePath { get; set; }
+        public Logger()
+        {
+            this.CurrentDirectory = Directory.GetCurrentDirectory();
+            this.FileName = "log.txt";
+            this.FilePath = this.CurrentDirectory + "/" + this.FileName;
+
+        }
+
+        public  void Log(string Messege)
+        {
+            using (System.IO.StreamWriter sw = System.IO.File.AppendText(this.FilePath)) 
+            {
+                sw.Write("\r\n Log Entry:");
+                sw.WriteLine("{0}{1}",DateTime.Now.ToLongTimeString(),DateTime.Now.ToLongDateString());
+                sw.WriteLine("{0}", Messege);
+
+            }
+           
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+            logger.Log("This is an");
+            logger.Log("logfille");
+        }
+    }
+}
