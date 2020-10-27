6f5f5b9567e6f0791506ed5bec92d4f476af4f81
sksahil152 <sksahil152@gmail.com>
2020-10-27 10:57:24 +0530

    DB First approach

---
+++ DBFirst 15-10-2020/DataBaseFirst/DataBaseFirst/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DBFirstEntityEntities();
+            var post = new Post()
+            {
+
+                //PostId = 1,
+               DatePublish = DateTime.Now,
+               Title="This is title",
+                Body = "This is body",
+              
+            };
+            try
+            {
+                context.Posts.Add(post);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+            Console.WriteLine("successful...!");
+            Console.ReadLine();
+        }
+    }
+}
